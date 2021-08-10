1bb3409d10befd3b53c074fb464cb825778d4fd6
linh97abc <linh97abc@gmail.com>
2021-08-10 17:58:36 +0700

    scripts: Update run_cov.bat
    Add scripts parse properties file
    
    Signed-off-by: linh97abc <linh97abc@gmail.com>

--- run_cov.bat
+++ run_cov.bat
@@ -1,16 +1,21 @@
 @echo off
 
-set V_SOURCE=uart.v uart_tx.v uart_rx.v uart_tb.v
-set TEST_MODULE=uart_tb
 set COV_REPORT=cov_report
 set LIB_NAME=work
+set PROPS_PATH=.
 
-echo -- Create a new library: %LIB_NAME%
-vlib %LIB_NAME%
-vmap %LIB_NAME% %LIB_NAME%
-echo -- Compile the design and testbench...
-vlog -coveropt 3 +cover +acc %V_SOURCE%
-echo -- Run the simulation for code coverage...
-vsim -coverage -vopt %LIB_NAME%.%TEST_MODULE% -c -do "coverage save -onexit -directive -codeAll %COV_REPORT%.ucdb;run -all"
-echo -- Export the code coverage to HTML file...
-vcover report -html %COV_REPORT%.ucdb
+:parsePropertiesFile
+    if NOT "%1"=="" PROPS_PATH=%1
+    set PROPS_FILE=%PROPS_PATH%\source.properties
+    FOR /F "tokens=*" %%i in ('type %PROPS_FILE% ^| findStr.exe "%1="') do SET %%i
+
+:main
+    echo -- Create a new library: %LIB_NAME%
+    vlib %LIB_NAME%
+    vmap %LIB_NAME% %LIB_NAME%
+    echo -- Compile the design and testbench...
+    vlog -coveropt 3 +cover +acc %V_SOURCE%
+    echo -- Run the simulation for code coverage...
+    vsim -coverage -vopt %LIB_NAME%.%TEST_MODULE% -c -do "coverage save -onexit -directive -codeAll %COV_REPORT%.ucdb;run -all"
+    echo -- Export the code coverage to HTML file...
+    vcover report -html %COV_REPORT%.ucdb
